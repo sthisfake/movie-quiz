7231972538f8a07b0b8c380dbde788b03e4d61f8
sthisfake <pouya.gachboy@gmail.com>
2022-08-13 00:44:43 +0430

    doing great

--- movieWebsite/Models/classes.py
+++ movieWebsite/Models/classes.py
@@ -1,5 +1,5 @@
-
-
+from validate_email_address import validate_email
+import re
 
 allUsers = []
 
@@ -51,5 +51,60 @@ class User():
     def getAllUsers(self):
         return allUsers           
 
+    def checkIfExistUsername(self):
+
+        #returns true if it exist 
+
+        sign = False
+        for user in allUsers:
+            if user.getUserName() == self.userName:
+                sign = True 
+                return sign
+        return sign        
+
+    def checkIfEmailExistDataBase(self):
+        #returns true if it exist in database
+        sign = False 
+        for user in allUsers:
+            if user.getEmail() == self.email:
+                sign = True
+                return sign
+        return sign         
+            
+    def checkEmailValidation(self):
+        #return True if the email address is valid 
+        isvalid=validate_email(self.email)
+        return isvalid
+
+    def checkEmailReallExistence(self):
+        #return True if the email address exists 
+        isExists = validate_email(self.email, verify=True) 
+        return isExists   
+
+    def checkIfPasswordIsVallid(self):
+        check = True
+        status = False
+        password = self.password
+        massage = ''
+        while check:  
+            if (len(password)<8):
+                massage = 'password too short'
+                break
+            elif not re.search("[a-z]",password):
+                massage = 'password must contain lower case charactors'
+                break
+            elif not re.search("[0-9]",password):
+                massage = 'password must contain a number'
+                break
+            elif not re.search("[A-Z]",password):
+                massage = 'password must contain upper case charactors'
+                break
+            else:
+                 check=False
+                 status = True
+                 break
+        return status , massage
+
+        
 
 
--- movieWebsite/Models/pages.py
+++ movieWebsite/Models/pages.py
@@ -1,7 +1,7 @@
 from cgitb import html
 import imp
 from urllib.parse import uses_relative
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from requests import request
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
@@ -12,6 +12,46 @@ def homePage(request) :
     return render(request , "home.html" , {} )    
 
 def signUp(request):
+
+    if request.method == 'POST':
+
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName' )
+        UserName = request.POST.get('UserName')
+        email = request.POST.get('email' )
+        password = request.POST.get('password')
+        user = User(firstName , lastName , UserName  , email , password )
+
+        checkUserName = user.checkIfExistUsername()
+        if checkUserName == True :
+            context = {'userError' : 'this user name already taken , try again'}
+            return render(request , 'signUp.html'  , context )
+
+
+        checkEmailExist = user.checkIfEmailExistDataBase()    
+        if checkEmailExist == True:
+            context = {'emailError' : 'this email has already have an account , try again'}
+            return render(request , 'signUp.html'  , context ) 
+
+        
+        checkEmailValidation = user.checkEmailValidation()   
+        if checkEmailValidation == False :
+            context = {'emailError' : 'please write a correct email'}
+            return render(request , 'signUp.html'  , context )      
+
+
+        checkEmailExistForReal = user.checkEmailReallExistence()   
+        if checkEmailExistForReal == False:
+            context = {'emailError' : 'this email doesnt exist '}
+            return render(request , 'signUp.html'  , context )  
+
+        isValid , massage = user.checkIfPasswordIsVallid()  
+        if isValid == False:
+            context = {'passError' : massage}
+            return render(request , 'signUp.html'  , context )  
+
+        user.addToUsers()
+        redirect('user')
     return render(request , "signUp.html" ,{} )    
 
 def login(request):
@@ -21,14 +61,6 @@ def login(request):
     return render(request , "login.html" , {})  
 
 def userPage(request):
-    if request.method == 'POST':
-        firstName = request.POST.get('firstName')
-        lastName = request.POST.get('lastName' )
-        UserName = request.POST.get('UserName')
-        email = request.POST.get('email' )
-        password = request.POST.get('password')
-        user = User(firstName , lastName , UserName  , email , password )
-        user.addToUsers()
     return render(request , 'user.html' , {})
 
 
